bc889b38ac776200416b509b1e8a24fc68a2b36b
Magnus Holm <judofyr@gmail.com>
2015-07-22 16:02:31 +0200

    Tweak deps:
    - Always build with -O3
    - Harfbuzz doesn’t like -c
    - No glib, no CoreText

--- builddeps.sh
+++ builddeps.sh
@@ -4,22 +4,18 @@ HARFBUZZ="$PWD/vendor/harfbuzz"
 FREETYPE2="$PWD/vendor/freetype2"
 GL3W="$PWD/vendor/gl3w"
 
-if [ "$1" = release ]; then
-  export CFLAGS="-c -O3"
-else
-  export CFLAGS="-c -g -O0"
-fi
-
+export CFLAGS="-c -O3"
 pushd $FREETYPE2
 sh autogen.sh
 make setup ansi
 make
 popd
 
+export CFLAGS="-O3"
 pushd $HARFBUZZ
 FREETYPE_CFLAGS="-I$FREETYPE2/include" \
 FREETYPE_LIBS="-L$FREETYPE2/objs -lfreetype" \
-sh autogen.sh --disable-shared --enable-static
+sh autogen.sh --disable-shared --enable-static --with-glib=no --with-coretext=no
 make
 popd
 
